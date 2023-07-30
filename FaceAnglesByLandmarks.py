6f528c1c8f4b5af68fb7ae09d1d4e59006f61d48
Reiyo OISHI <reiyo.icepp@gmail.com>
2023-07-31 01:00:42 +0900

    Updated

--- FaceAnglesByLandmarks.py
+++ FaceAnglesByLandmarks.py
@@ -30,7 +30,7 @@ COLOR_PINK   = (255, 0,   255)
 COLOR_YELLOW = (0,   255, 255)
 COLOR_WHITE  = (255, 255, 255)
 
-class MediaPipePnPLight(object):
+class MediaPipePnP(object):
     '''
     PnP = MediaPipePnPLight(width, height)
     PnP.parse_detected_facial_points_2d() # Reset face landmarks
@@ -125,89 +125,6 @@ class MediaPipePnPLight(object):
 
         return (roll, pitch, yaw)
 
-class MediaPipePnP(object):
-    def __init__(self, image_width: int, image_height: int, landmarks,
-                 landmark_indices: list = [
-                     1, # Nose
-                     33, 263, # Left, right eye
-                     61, 291, # Left, right mouth
-                     199, # Chin
-                     168, # between the eyebrows
-                     17, # bottom lip
-                     101, 330, # Left, right cheek
-                     234, 454, # Left, right ear
-                ]):
-        # Features of input image
-        self.width = image_width
-        self.height = image_height
-
-        # Face landmarks (canonical points & detected points)
-        self.landmarks = landmarks
-        self.facial_point_file = 'canonical_face_model/canonical_face_model.obj'
-        self.facial_points_3d = {}
-        self._parse_canonical_facial_points_3d()
-        self.facial_points_2d = {}
-        self._parse_detected_facial_points_2d()
-
-        # Indices for PnP
-        self.landmarks_indices = landmark_indices
-        self.cam_matrix = np.array([
-            [image_width,           0,  image_width / 2],
-            [          0, image_width, image_height / 2],
-            [          0,           0,                1],
-        ])
-        self.distortion_matrix = np.zeros((4, 1))
-
-    def _parse_canonical_facial_points_3d(self):
-        logger.info('Parse canonical facial 3d points')
-        with open(self.facial_point_file, mode = 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                elements = line.split()
-                if elements[0] == 'v':
-                    self.facial_points_3d[len(self.facial_points_3d)] = \
-                        (float(elements[1]), float(elements[2]), float(elements[3].replace('\n', '')))
-
-    def _parse_detected_facial_points_2d(self):
-        logger.debug('Parse detected facial 2d points')
-        for i_landmark, landmark in enumerate(self.landmarks):
-            self.facial_points_2d[i_landmark] = (landmark.x * self.width, landmark.y * self.height)
-
-    def perspective_n_points(self):
-        points_3d_cal = np.array([self.facial_points_3d[index] for index in self.landmarks_indices])
-        points_2d_cal = np.array([self.facial_points_2d[index] for index in self.landmarks_indices])
-
-        success, vector_rotation, vector_translation = \
-            cv2.solvePnP(points_3d_cal, points_2d_cal, self.cam_matrix, self.distortion_matrix,
-                         flags = cv2.SOLVEPNP_ITERATIVE) # SOLVEPNP_EPNP or SOLVEPNP_ITERATIVE
-        
-        return (success, vector_rotation, vector_translation)
-    
-    def project_points_used_in_pnp(self):
-        points_3d_cal = np.array([self.facial_points_3d[index] for index in self.landmarks_indices])
-        points_2d_cal = np.array([self.facial_points_2d[index] for index in self.landmarks_indices])
-
-        _, rotation, translation = self.perspective_n_points()
-        facial_end_points_2d, _ = \
-            cv2.projectPoints(points_3d_cal, rotation, translation, self.cam_matrix, self.distortion_matrix)
-        
-        return (points_2d_cal, facial_end_points_2d)
-
-    def project_given_points(self, points_3d):
-        _, rotation, translation = self.perspective_n_points()
-        facial_end_points_2d, _ = \
-            cv2.projectPoints(points_3d, rotation, translation, self.cam_matrix, self.distortion_matrix)
-        return facial_end_points_2d
-
-    def rot_params_rv(self):
-        from math import pi, atan2, asin
-        _, rotation, _ = self.perspective_n_points()
-        R = cv2.Rodrigues(rotation)[0]
-        roll = 188 * atan2(-R[2][1], R[2][2]) / pi
-        pitch = 180 * asin(R[2][0]) / pi
-        yaw = 180 * atan2(-R[1][0], R[0][0]) / pi
-        return (roll, pitch, yaw)
-
 class FaceLandmarkForVideo(object):
     def __init__(self, input_video: str, output_video: str):
         # Input video attributes
@@ -244,26 +161,20 @@ class FaceLandmarkForVideo(object):
         self.landmarker = face_landmarker.create_from_options(options)
 
         # Perspective-N-Points
-        self.PnP = MediaPipePnPLight(width = self.width, height = self.height)
+        self.PnP = MediaPipePnP(width = self.width, height = self.height)
 
     def decorate_frame(self, frame, landmarks):
         self.PnP.parse_detected_facial_points_2d(landmarks = landmarks)
-        points_2d_cal, projected_points_2d = self.PnP.project_points_used_in_pnp()
+        points_2d_cal, projected_points_2d_cal = self.PnP.project_points_used_in_pnp()
 
         # Drawing calculation points and projected points
         for p in points_2d_cal:
             cv2.circle(
-                frame,
-                (int(p[0]), int(p[1])),
-                3, COLOR_WHITE, 2
-            )
+                frame, (int(p[0]), int(p[1])), 3, COLOR_WHITE, 2)
 
-        for p in projected_points_2d:
+        for p in projected_points_2d_cal:
             cv2.circle(
-                frame,
-                (int(p[0][0]), int(p[0][1])),
-                3, COLOR_PINK, 2
-            )
+                frame, (int(p[0][0]), int(p[0][1])), 3, COLOR_PINK, 2)
 
         # Drawing X,Y,Z-axis in a frame from the nose point
         given_points_3d = np.array([
@@ -272,26 +183,26 @@ class FaceLandmarkForVideo(object):
             ( 0,  0, 10),
             ( 0,  0,  0),
         ], dtype = np.float32)
-        projected_points_2d = self.PnP.project_given_points(points_3d = given_points_3d)
-        diff_x = points_2d_cal[0][0] - projected_points_2d[3][0][0]
-        diff_y = points_2d_cal[0][1] - projected_points_2d[3][0][1]
+        projected_points_2d_cal = self.PnP.project_given_points(points_3d = given_points_3d)
+        diff_x = points_2d_cal[0][0] - projected_points_2d_cal[3][0][0]
+        diff_y = points_2d_cal[0][1] - projected_points_2d_cal[3][0][1]
 
         cv2.line(
             frame,
-            (int(projected_points_2d[3][0][0] + diff_x), int(projected_points_2d[3][0][1] + diff_y)),
-            (int(projected_points_2d[0][0][0] + diff_x), int(projected_points_2d[0][0][1] + diff_y)),
+            (int(projected_points_2d_cal[3][0][0] + diff_x), int(projected_points_2d_cal[3][0][1] + diff_y)),
+            (int(projected_points_2d_cal[0][0][0] + diff_x), int(projected_points_2d_cal[0][0][1] + diff_y)),
             COLOR_BLUE, 4
         )
         cv2.line(
             frame,
-            (int(projected_points_2d[3][0][0] + diff_x), int(projected_points_2d[3][0][1] + diff_y)),
-            (int(projected_points_2d[1][0][0] + diff_x), int(projected_points_2d[1][0][1] + diff_y)),
+            (int(projected_points_2d_cal[3][0][0] + diff_x), int(projected_points_2d_cal[3][0][1] + diff_y)),
+            (int(projected_points_2d_cal[1][0][0] + diff_x), int(projected_points_2d_cal[1][0][1] + diff_y)),
             COLOR_GREEN, 4
         )
         cv2.line(
             frame,
-            (int(projected_points_2d[3][0][0] + diff_x), int(projected_points_2d[3][0][1] + diff_y)),
-            (int(projected_points_2d[2][0][0] + diff_x), int(projected_points_2d[2][0][1] + diff_y)),
+            (int(projected_points_2d_cal[3][0][0] + diff_x), int(projected_points_2d_cal[3][0][1] + diff_y)),
+            (int(projected_points_2d_cal[2][0][0] + diff_x), int(projected_points_2d_cal[2][0][1] + diff_y)),
             COLOR_RED, 4
         )
 
@@ -346,13 +257,75 @@ class FaceLandmarkForImage(object):
             output_face_blendshapes = True,
             output_facial_transformation_matrixes = True,
         )
-        
+
         self.landmarker = face_landmarker.create_from_options(options)
+
+        # Perspective-N-Points
+        self.PnP = MediaPipePnP(width = self.width, height = self.height)
+
+    def decorate_frame(self, frame, landmarks):
+        self.PnP.parse_detected_facial_points_2d(landmarks = landmarks)
+        points_2d_cal, projected_points_2d_cal = self.PnP.project_points_used_in_pnp()
+
+        # Drawing calculation points and projected points
+        for p in points_2d_cal:
+            cv2.circle(
+                frame, (int(p[0]), int(p[1])), 2, COLOR_WHITE, 2)
+
+        for p in projected_points_2d_cal:
+            cv2.circle(
+                frame, (int(p[0][0]), int(p[0][1])), 3, COLOR_PINK, 2)
+
+        # Drawing X,Y,Z-axis in a frame from the nose point
+        given_points_3d = np.array([
+            (10,  0,  0),
+            ( 0, 10,  0),
+            ( 0,  0, 10),
+            ( 0,  0,  0),
+        ], dtype = np.float32)
+        projected_points_2d_cal = self.PnP.project_given_points(points_3d = given_points_3d)
+        diff_x = points_2d_cal[0][0] - projected_points_2d_cal[3][0][0]
+        diff_y = points_2d_cal[0][1] - projected_points_2d_cal[3][0][1]
+
+        cv2.line(
+            frame,
+            (int(projected_points_2d_cal[3][0][0] + diff_x), int(projected_points_2d_cal[3][0][1] + diff_y)),
+            (int(projected_points_2d_cal[0][0][0] + diff_x), int(projected_points_2d_cal[0][0][1] + diff_y)),
+            COLOR_BLUE, 4
+        )
+        cv2.line(
+            frame,
+            (int(projected_points_2d_cal[3][0][0] + diff_x), int(projected_points_2d_cal[3][0][1] + diff_y)),
+            (int(projected_points_2d_cal[1][0][0] + diff_x), int(projected_points_2d_cal[1][0][1] + diff_y)),
+            COLOR_GREEN, 4
+        )
+        cv2.line(
+            frame,
+            (int(projected_points_2d_cal[3][0][0] + diff_x), int(projected_points_2d_cal[3][0][1] + diff_y)),
+            (int(projected_points_2d_cal[2][0][0] + diff_x), int(projected_points_2d_cal[2][0][1] + diff_y)),
+            COLOR_RED, 4
+        )
+
+        roll, pitch, yaw = self.PnP.get_roll_pitch_yaw()
+        cv2.putText(
+            frame,
+            text = 'Roll: %.4f, Pitch: %.4f, Yaw: %.4f' % (roll, pitch, yaw),
+            org = (int(landmarker.width * 0.05), int(landmarker.height * 0.05)),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1.0,
+            color=COLOR_BLACK,
+            thickness=3,
+            lineType=cv2.LINE_4)
+
+        cv2.imshow('Final', frame)
+        cv2.waitKey(0)
 
     def check_image(self):
         mp_frame = mp.Image(image_format = mp.ImageFormat.SRGB, data = self.input_image)
         results = self.landmarker.detect(mp_frame)
-        return results
+
+        if len(results.face_landmarks) > 0:
+            self.decorate_frame(self.input_image, results.face_landmarks[0])
 
     def plot_3d_landmarks(self):
         face_landmarks_list = self.check_image().face_landmarks
@@ -377,6 +350,10 @@ class FaceLandmarkForImage(object):
 
         plt.savefig(self.output_image_name)
 
+    def release_all(self):
+        cv2.destroyAllWindows()
+        logger.info('All jobs done.')
+
 if __name__ == '__main__':
     start_time = time.time()
 
@@ -391,7 +368,7 @@ if __name__ == '__main__':
     end_time = time.time()
     logger.info('Duration: %.4f sec' % (end_time - start_time))
 
-    sys.exit(1)
+    # sys.exit(1)
 
     start_time = time.time()
 
@@ -401,60 +378,11 @@ if __name__ == '__main__':
     # base_name = 'Gaki'
     input_image = '../Inputs/Images/%s.jpg' % (base_name)
     output_image = '../Outputs/Images/%s_landmark3D.jpg' % (base_name)
-    landmarker = FaceLandmarkForImage(input_image=input_image, output_image=output_image)
-    results = landmarker.check_image()
 
-    # Perspective-N-points
-    mp_pnp = MediaPipePnP(image_width=landmarker.width, image_height=landmarker.height,
-                          landmarks=results.face_landmarks[0])
-    points_2d_cal, end_points_2d = mp_pnp.project_points_used_in_pnp()
-
-    for p in points_2d_cal:
-        cv2.circle(landmarker.input_image,
-                   (int(p[0]), int(p[1])),
-                   3, COLOR_WHITE, 2)
-
-    for p in end_points_2d:
-        cv2.circle(landmarker.input_image,
-                   (int(p[0][0]), int(p[0][1])),
-                   3, COLOR_PINK, 2)
-
-    given_points_3d = np.array([
-        (10, 0,  0),
-        (0, 10,  0),
-        (0,  0, 10),
-        (0,  0,  0),
-    ], dtype = np.float32)
-    projected_points_2d = mp_pnp.project_given_points(points_3d = given_points_3d)
-    diff_x = points_2d_cal[0][0] - projected_points_2d[3][0][0]
-    diff_y = points_2d_cal[0][1] - projected_points_2d[3][0][1]
-
-    cv2.line(landmarker.input_image,
-             (int(projected_points_2d[3][0][0] + diff_x), int(projected_points_2d[3][0][1] + diff_y)),
-             (int(projected_points_2d[0][0][0] + diff_x), int(projected_points_2d[0][0][1] + diff_y)),
-             COLOR_BLUE, 4)
-    cv2.line(landmarker.input_image,
-             (int(projected_points_2d[3][0][0] + diff_x), int(projected_points_2d[3][0][1] + diff_y)),
-             (int(projected_points_2d[1][0][0] + diff_x), int(projected_points_2d[1][0][1] + diff_y)),
-             COLOR_GREEN, 4)
-    cv2.line(landmarker.input_image,
-             (int(projected_points_2d[3][0][0] + diff_x), int(projected_points_2d[3][0][1] + diff_y)),
-             (int(projected_points_2d[2][0][0] + diff_x), int(projected_points_2d[2][0][1] + diff_y)),
-             COLOR_RED, 4)
-
-    roll, pitch, yaw = mp_pnp.rot_params_rv()
-    cv2.putText(landmarker.input_image,
-                text = 'Roll: %.4f, Pitch: %.4f, Yaw: %.4f' % (roll, pitch, yaw),
-                org = (int(landmarker.width * 0.05), int(landmarker.height * 0.05)),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                fontScale=1.0,
-                color=COLOR_BLACK,
-                thickness=3,
-                lineType=cv2.LINE_4)
-
-    cv2.imshow('Final', landmarker.input_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #landmarker = FaceLandmarkForImage(input_image=input_image, output_image=output_image)
+    #landmarker.check_image()
+    # landmarker.plot_3d_landmarks()
+    #landmarker.release_all()
 
     end_time = time.time()
     logger.info('Duration: %.4f sec' % (end_time - start_time))
